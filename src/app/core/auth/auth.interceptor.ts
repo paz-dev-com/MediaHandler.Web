a52a8ff53b8e61801db3a132ecc7def53e59318e
Tibo Pfeifer <tibo.pfeifer@gmail.com>
2026-03-07 18:14:58 +0100

    fix: Auth0 authentication

--- src/app/core/auth/auth.interceptor.ts
+++ src/app/core/auth/auth.interceptor.ts
@@ -1,18 +1,20 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
+import { environment } from '@env/environment';
 import { from, switchMap } from 'rxjs';
 import { AuthService } from './auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  if (!req.url.startsWith(environment.apiBaseUrl)) {
+    return next(req);
+  }
+
   const authService = inject(AuthService);
 
   return from(authService.getAccessToken()).pipe(
     switchMap((token) => {
       if (token) {
-        const authReq = req.clone({
-          setHeaders: { Authorization: `Bearer ${token}` },
-        });
-        return next(authReq);
+        return next(req.clone({ setHeaders: { Authorization: `Bearer ${token}` } }));
       }
       return next(req);
     }),
